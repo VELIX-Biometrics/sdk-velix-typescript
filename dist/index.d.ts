817e5715b44e05fe6482744c5665f30937cef656
Fábio Rodrigues (Brajola) <brajola@gmail.com>
2026-06-22 07:31:56 -0300

    chore: adiciona .gitignore, remove node_modules e dist do tracking

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { VelixClient } from './client';
-export type { VelixClientConfig } from './client';
-export { CheckinModule } from './modules/checkin';
-export { PersonsModule } from './modules/persons';
-export type { CheckinResult, Person, PaginatedResult } from './types';
